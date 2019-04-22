565c572caad19591a315ec531d2a6b1a539ef429
80son <mrdbar@gmail.com>
2019-04-22 07:31:58 -0400

    First draft of Messageboard Code

--- Lesson-2/3_MessageboardPartOne/MessageboardPartOne.py
+++ Lesson-2/3_MessageboardPartOne/MessageboardPartOne.py
@@ -24,14 +24,14 @@ from urllib.parse import parse_qs
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         # 1. How long was the message? (Use the Content-Length header.)
-
+        length = int(self.headers.get('Content-Length', 0))
         # 2. Read the correct amount of data from the request.
-
+        data = self.rfile.read(length).decode()
         # 3. Extract the "message" field from the request data.
-
+        message = urlparse(data)
         # Send the "message" field back as the response.
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.send_huader('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
         self.wfile.write(message.encode())
 
